02cba114d8cda90e9129502579dd3ffb7636dda2
Zakarya <zakarya@colormatic.org>
2025-04-18 13:45:49 -0700

    De-lobotomize the Lua Language Server
    The AI code was wrong
    Now here is some different AI code

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -1,22 +1,5 @@
 return {
 	{
 		"dense-analysis/ale",
-		config = function()
-			-- Make the Lua LS not freak out about the undefined globals
-			vim.g.ale_linters = {
-				lua = { 'sumneko_lua' },
-			}
-
-			vim.g.ale_lua_sumneko_lua_executable = 'lua-language-server'
-			vim.g.ale_lua_sumneko_lua_options = {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { 'vim', 'Snacks' },
-						},
-					},
-				},
-			}
-		end
 	},
 }
