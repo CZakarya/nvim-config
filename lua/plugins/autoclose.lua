24bf025e9e46feba2f944aa7403fb5e8f68a326b
Zakarya <zakarya@colormatic.org>
2025-05-05 17:16:10 -0700

    Add rational behind disabling single quote autoclosing

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -4,6 +4,11 @@ return {
 		config = function()
 			require("autoclose").setup({
 				keys = {
+					--[[**
+					* Here I'm removing the single quote "tick" character from autoclose
+					* because I almost never use single quotes for string literals, but
+					* I often use them as apostrophes (which should not be autoclosed).
+					*]]
 					["'"] = { escape = false, close = false, pair = "''" },
 				},
 			})
