d02cc4c2116d627f5c130486e6bc0e65b2d54e49
Zakarya <zakarya@colormatic.org>
2025-08-06 16:58:04 -0700

    Tabs

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		opts = {
+			options = {
+				always_show_bufferline = false,
+				offsets = {
+					{
+						filetype = "snacks_layout_box",
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			require("bufferline").setup(opts)
+		end
+	},
+}
