90ab3b681625e56863307b225eec5d88dfb75a30
Zakarya <zakarya@colormatic.org>
2025-10-06 16:40:47 -0700

    Slanted tabs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,15 +6,22 @@ return {
 		opts = {
 			options = {
 				always_show_bufferline = false,
+				separator_style = "slope",
+				diagnostics = "nvim_lsp",
 				offsets = {
 					{
 						filetype = "snacks_layout_box",
 					},
 				},
+				hover = {
+					enable = true,
+					delay = 0,
+					reveal = { "close" },
+				},
 			},
 		},
 		config = function(_, opts)
 			require("bufferline").setup(opts)
-		end
+		end,
 	},
 }
