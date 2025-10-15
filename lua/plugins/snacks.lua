55e4fd829396166c9834c12dca3c74c35564a7e3
Zakarya <zakarya@colormatic.org>
2025-10-15 15:06:34 -0700

    Custom dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,7 +5,17 @@ return {
 		lazy = false,
 		opts = {
 			bigfile = { enabled = true },
-			dashboard = { enabled = true },
+			dashboard = {
+				enabled = true,
+				sections = {
+					{ section = "header" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene" },
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+					{ gap = 1 },
+					{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				},
+			},
 			explorer = { enabled = true },
 			indent = { enabled = true },
 			input = { enabled = true },
