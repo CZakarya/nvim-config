aba1aea0596402759fd280002ff12c3164a03f58
Zakarya <zakarya@colormatic.org>
2026-03-28 16:51:51 -0700

    Add transparent.nvim

---
+++ lua/plugins/transparent.lua
@@ -0,0 +1,48 @@
+return {
+	{
+		"xiyaowong/transparent.nvim",
+		opts = {
+			groups = {
+				"Normal",
+				"NormalNC",
+				"Comment",
+				"Constant",
+				"Special",
+				"Identifier",
+				"Statement",
+				"PreProc",
+				"Type",
+				"Underlined",
+				"Todo",
+				"String",
+				"Function",
+				"Conditional",
+				"Repeat",
+				"Operator",
+				"Structure",
+				"LineNr",
+				"NonText",
+				"SignColumn",
+				"CursorLine",
+				"CursorLineNr",
+				"StatusLine",
+				"StatusLineNC",
+				"EndOfBuffer",
+			},
+			-- table: additional groups that should be cleared
+			extra_groups = {},
+			-- table: groups you don't want to clear
+			exclude_groups = {},
+			-- function: code to be executed after highlight groups are cleared
+			-- Also the user event "TransparentClear" will be triggered
+			on_clear = function() end,
+		},
+		config = function(_, opts)
+			require("transparent").setup(opts)
+
+			require("transparent").clear_prefix("Snacks")
+			require("transparent").clear_prefix("lualine")
+			-- require("transparent").clear_prefix("BufferLine") -- Doesn't work
+		end,
+	},
+}
